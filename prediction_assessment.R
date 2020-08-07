498be5acbce9b1fb0c9a49de4340f84997ce1f14
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-08-07 12:34:54 -0500

    first commit script prediction assessment in RLM

---
+++ prediction_assessment.R
@@ -0,0 +1,8 @@
+# Evaluación de la Predicción en RLM
+# Instalación de paquetes
+# install.packages("caret")
+dir <- "/path/project_directory/"
+setwd(dir)
+# Cargamos el conjunto de datos a la variable dataset
+path_dataset <- "datasets/datos2.csv"
+dataset <- read.csv(path_dataset)
